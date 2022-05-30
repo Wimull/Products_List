6ab4041b5a2c5d9aa16a03bd66ded3f96493145c
Wimull <aninhasilva@tutanota.com>
2022-05-29 22:09:22 -0300

    Fixed some styling issues with footer

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
 	return (
 		<div
 			id="Footer"
-			className="d-flex text-center border-top border-5 border-dark py-5 mt-5 justify-content-center align-items-center"
+			className="d-flex justify-content-center align-items-center text-center border-top border-5 border-dark py-5 mt-5 "
 		>
 			<span className="">Scandiweb Test assignment</span>
 		</div>
